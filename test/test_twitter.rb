68fc873e1ab60d919307f22904a54fadcfa7613a
victortyau <victortyau@gmail.com>
2011-04-19 13:44:24 -0700

    uploading changes

--- test/test_twitter.rb
+++ test/test_twitter.rb
@@ -1,5 +1,15 @@
-require 'twitter'
+require "twitter"
 
-#puts "my current location "+Twitter.user("sferik").location
+ Twitter.user_timeline("parrot_project",:page => 1).each do | h | 
+	puts h.text
+end
 
-#puts "hola"
+Twitter.configure do |config|
+  config.consumer_key = "B9aOm7lxTUChbg2jXmaw"
+  config.consumer_secret = "JfPabhEfRbPOwScFsHUj42q6XcVIRac9UpGig2W5VZE"
+  config.oauth_token = "284132864-YZTcAHSdWHYMKtTU7YcPtAcjYTG5jM9emoCkGQuq"
+  config.oauth_token_secret = "rcv5dJTwF1VKPUaChoCp8EiRq01JCJxt7KoCZeS0"
+end
+
+
+puts Twitter.direct_messages
